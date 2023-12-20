f57decb69c2f9a611398560d87d1792f65a6e696
snrakshith <snrakshith.97@gmail.com>
2023-12-20 17:37:31 +0530

    feat: add other test route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.get("/test", (req: Request, res: Response) => {
+  res.json({ status: 200, message: "Running Successfully" });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ status: 200, message: "Fine!!" });
 });
